4933cadafa82a3f239a523ebf206da005e77db5c
Eduardo Diniz <diniz480@gmail.com>
2022-11-04 17:23:02 -0300

    att

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input()
+  bigCardTitle:string = '';
+  @Input()
+  bigCardImage:string = '';
+  @Input()
+  bigCardDesc:string = ''
+
 
   constructor() { }
 
